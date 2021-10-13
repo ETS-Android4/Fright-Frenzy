2b4c7aa3bb310a5604469cbe2b7946c93eaaf6cf
Clarekpd <Clarekpd@outlook.com>
2021-10-12 20:20:49 -0500

    tensor flow program updates

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TensorFlow.java
@@ -0,0 +1,67 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+import org.firstinspires.ftc.robotcore.external.ClassFactory;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
+import org.firstinspires.ftc.robotcore.external.tfod.TFObjectDetector;
+
+import java.util.List;
+
+class TensorFlow {
+
+    private OpMode op;
+    
+    private static final String TFOD_MODEL_ASSET = "FreightFrenzy_BCDM.tflite";
+    private static final String[] LABELS = {
+            "Ball",
+            "Cube",
+            "Duck",
+            "Marker"
+    };
+    private static final String VUFORIA_KEY =
+            "AcUwNff/////AAABmSDjtLt8eEYos7+P16Q5uMpfai9UDhOe3GPRF9oLweSCr+ydsB1z1O07EAL8u6QzfDIp2DKNqqxD7AzaTUEjHxLeL/W86upMAQ/yj+i0xCTmb46d6WyaCEK//pGA1eXtYAUzXizSQiLvp3ljz1d27Lv8xsJb+RQqFRW+IgJ/k+McoNBZF6v9Y+huXNSZhUtfqklrr4IhP64h9DGxrAst7swmUES4fsMGXRAF+p2sJlgv9cKtJpdKo6e0xSbN5Oe4+0nGKhvwIO7qGW4tQLM/1h1VlJoQWhT8N42Ccho4cu83IcFN1WIqSpz7KsGEuqEonrf2S4tthqMJ+FE5f4cYY0jgfkxaPf4y0GjKR0cggrXSv";
+    private VuforiaLocalizer vuforia;
+    private TFObjectDetector tfod;
+
+    public void turnOn() {
+        // TODO: turn on tensor flow
+        VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters();
+
+        parameters.vuforiaLicenseKey = VUFORIA_KEY;
+        parameters.cameraName = op.hardwareMap.get(WebcamName.class, "Webcam 1");
+        
+        vuforia = ClassFactory.getInstance().createVuforia(parameters);
+
+        int tfodMonitorViewId = op.hardwareMap.appContext.getResources().getIdentifier(
+                "tfodMonitorViewId", "id", op.hardwareMap.appContext.getPackageName());
+        TFObjectDetector.Parameters tfodParameters = new TFObjectDetector.Parameters(tfodMonitorViewId);
+        tfodParameters.minResultConfidence = 0.8f;
+        tfodParameters.isModelTensorFlow2 = true;
+        tfodParameters.inputSize = 320;
+        tfod = ClassFactory.getInstance().createTFObjectDetector(tfodParameters, vuforia);
+        tfod.loadModelFromAsset(TFOD_MODEL_ASSET, LABELS);
+
+        if (tfod != null) {
+            tfod.activate();
+            tfod.setZoom(2.5, 16.0/9.0);
+        }
+    }
+
+    public void turnOff() {
+        // TODO: turn off tensor flow
+        tfod.shutdown();
+        tfod.deactivate();
+    }
+
+    public List<Recognition> getRecognitions() {
+        // TODO: get the list of Recognitions from tfod
+        List<Recognition> updatedRecognitions = null;
+        if (tfod != null) {
+            updatedRecognitions = tfod.getUpdatedRecognitions();
+        }
+        return updatedRecognitions;
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/DriveYeet.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/DriveYeet.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode.util;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp
@@ -9,13 +10,48 @@ public class DriveYeet extends LinearOpMode {
     private Servo servo0;
     private Servo servo1;
     private double servSpeed1;
+    private DcMotor backLeftMotor;
+    private DcMotor frontLeftMotor;
+    private DcMotor backRightMotor;
+    private DcMotor frontRightMotor;
 
     @Override
     public void runOpMode() throws InterruptedException {
         servo0 = hardwareMap.get(Servo.class, "servo0");
         servo1 = hardwareMap.get(Servo.class, "servo1");
+        backLeftMotor = hardwareMap.get(DcMotor.class, "motor0");
+        frontLeftMotor = hardwareMap.get(DcMotor.class, "motor1");
+        frontRightMotor = hardwareMap.get(DcMotor.class, "motor2");
+        backRightMotor = hardwareMap.get(DcMotor.class, "motor3");
+
+        frontLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
         waitForStart();
         while (opModeIsActive()){
+
+            double rightX_G1;
+            double rightY_G1;
+            double leftX_G1;
+            double leftY_G1;
+
+            rightY_G1 = -gamepad1.right_stick_y;
+            rightX_G1 = -gamepad1.right_stick_x;
+            leftY_G1 = gamepad1.left_stick_y;
+            leftX_G1 = -gamepad1.left_stick_x;
+
+            double frontLeft = (rightX_G1 + rightY_G1 - leftX_G1);
+            double backLeft = (rightX_G1 + rightY_G1 + leftX_G1);
+            double backRight = (rightX_G1 - rightY_G1 + leftX_G1);
+            double frontRight = (rightX_G1 - rightY_G1 - leftX_G1);
+
+            frontLeftMotor.setPower(frontLeft);
+            backLeftMotor.setPower(backLeft);
+            backRightMotor.setPower(backRight);
+            frontRightMotor.setPower(frontRight);
+
             if(gamepad1.right_bumper) {
                 servo0.setPosition(1);
             } else {
