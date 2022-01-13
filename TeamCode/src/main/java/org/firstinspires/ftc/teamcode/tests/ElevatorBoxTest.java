dfbf3c625b3fab6e384cdbd982dd54b644fc45a2
Andy Fisher <afisher2@users.noreply.github.com>
2022-01-13 13:25:41 -0600

    cargo box testing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/ElevatorBoxTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/ElevatorBoxTest.java
@@ -19,20 +19,37 @@ public class ElevatorBoxTest extends LinearOpMode {
         slide.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         waitForStart();
 
+        boolean timerRunning = false;
+        Timer timer = new Timer();
         while (opModeIsActive()) {
             double distance = distanceSensor.getDistance(DistanceUnit.INCH);
 
+            /*
             if (gamepad2.dpad_up && distance < 4) {
                 slide.setPower(0.3);
             } else if (gamepad2.dpad_up && distance >= 4) {
                 slide.setPower(1.0);
-            } else if (gamepad2.dpad_down && distance >= 2.5) {
+            }
+*/
+            if(!gamepad2.dpad_up) {
+                timerRunning = false;
+            } else if(gamepad2.dpad_up && !timerRunning && distance <= 3.0) {
+                timerRunning = true;
+                timer.start(1500);
+            }
+
+            if(gamepad2.dpad_up && timer.check()) {
+                slide.setPower(1.0);
+            } else if (gamepad2.dpad_down && distance >= 2.0) {
                 slide.setPower(-1);
             } else {
                 slide.setPower(0);
             }
 
-            if (gamepad2.y && distance > 2.5 && cargo.getPosition() < 0.6){
+            if(gamepad2.dpad_up || gamepad2.dpad_down) {
+                cargo.setPosition(0.35); // safe pose
+            }
+            else if (gamepad2.y && distance > 2.5 && cargo.getPosition() < 0.6){
                 cargo.setPosition(Servo.MIN_POSITION); // drop pos
             }
             else if(distance < 2.5) {
@@ -45,6 +62,23 @@ public class ElevatorBoxTest extends LinearOpMode {
             telemetry.addData("distance:", distance);
             telemetry.addData("box servo:", cargo.getPosition());
             telemetry.update();
+        }
+    }
+
+    class Timer {
+        long end;
+        boolean exp;
+        public void start(long duration) {
+            end = System.currentTimeMillis() + duration;
+            exp = false;
+        }
+
+        public boolean check() {
+            if(!exp) {
+                exp = System.currentTimeMillis() >= end;
+            }
+
+            return exp;
         }
     }
 }
